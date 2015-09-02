a1b388972d72ce071eedb2eb5ea5a8d102eee2a5
Romain Dardour <romain@hull.io>
2015-09-02 18:23:00 +0200

    Updates

--- ship.js
+++ ship.js
@@ -1,4 +1,4 @@
 !function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):"object"==typeof exports?exports["Hull Ships Repository"]=t():e["Hull Ships Repository"]=t()}(this,function(){return function(e){function t(r){if(n[r])return n[r].exports;var o=n[r]={exports:{},id:r,loaded:!1};return e[r].call(o.exports,o,o.exports,t),o.loaded=!0,o.exports}var n={};return t.m=e,t.c=n,t.p="/",t(0)}([/*!*********************!*\
   !*** ./src/ship.js ***!
   \*********************/
-function(e,t){"use strict"}])});
+function(e,t,n){"use strict"}])});
