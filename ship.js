19467ed3fcadedaf0ab785aa4c73e60fdf386124
Romain Dardour <romain@hull.io>
2016-07-08 20:14:33 +0200

    Updates

--- ship.js
+++ ship.js
@@ -1,1 +1,4 @@
-!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports["HullHull Ships RepositoryShip"]=t():e["HullHull Ships RepositoryShip"]=t()}(this,function(){return function(e){function t(r){if(o[r])return o[r].exports;var p=o[r]={exports:{},id:r,loaded:!1};return e[r].call(p.exports,p,p.exports,t),p.loaded=!0,p.exports}var o={};return t.m=e,t.c=o,t.p="/",t(0)}([function(e,t){"use strict"}])});
+!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):"object"==typeof exports?exports["HullHull Ships RepositoryShip"]=t():e["HullHull Ships RepositoryShip"]=t()}(this,function(){return function(e){function t(r){if(n[r])return n[r].exports;var o=n[r]={exports:{},id:r,loaded:!1};return e[r].call(o.exports,o,o.exports,t),o.loaded=!0,o.exports}var n={};return t.m=e,t.c=n,t.p="/",t(0)}([/*!*********************!*\
+  !*** ./src/ship.js ***!
+  \*********************/
+function(e,t,n){"use strict"}])});
